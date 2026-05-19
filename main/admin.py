3d5b850dea5195a50dc6419a9306797346373672
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-05-19 14:06:57 +0500

    Reformat admin.py for better readability

--- main/admin.py
+++ main/admin.py
@@ -2,18 +2,15 @@ from django.contrib import admin
 
 from .models import Airplane, Airport, Booking, Flight, Passenger
 
-
 @admin.register(Airplane)
 class AirplaneAdmin(admin.ModelAdmin):
     list_display = ("name", "model", "capacity")
     search_fields = ("name", "model")
 
-
 @admin.register(Airport)
 class AirportAdmin(admin.ModelAdmin):
     list_display = ("name", "city", "code")
     search_fields = ("name", "city", "code")
-
 
 @admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
@@ -28,12 +25,10 @@ class FlightAdmin(admin.ModelAdmin):
     search_fields = ("flight_number",)
     list_filter = ("departure_airport", "arrival_airport")
 
-
 @admin.register(Passenger)
 class PassengerAdmin(admin.ModelAdmin):
     list_display = ("name", "email")
     search_fields = ("name", "email")
-
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
